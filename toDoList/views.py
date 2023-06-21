7cdc162ef09272113a4bf7b245e3c066196003c9
wuryantoAji <ajiw4056@gmail.com>
2023-06-21 21:20:30 +0800

    done basic funtionality

--- toDoList/views.py
+++ toDoList/views.py
@@ -54,8 +54,8 @@ def allItemForTheList(request, toDoListId):
     toDoList = get_object_or_404(ToDoList, pk=toDoListId)
     toDoItem = ToDoItem.objects.filter(to_do_list_id=toDoListId)
     context = {
-        'todo_list' : toDoList,
-        'toDoItem' : toDoItem,
+        'todo_list': toDoList,
+        'toDoItem': toDoItem,
     }
     return render(request, 'toDoList/todo_list.html', context)
 
@@ -90,3 +90,40 @@ def addToDoListItem(request, toDoListId):
             return HttpResponseRedirect(reverse('toDoListDetail', args=[toDoListId]))
     else:
         return render(request, 'toDoItem/todolistitem_form.html', context)
+
+def updateToDoListItem(request, toDoListId, toDoItemId):
+    responseList = []
+    toDoItem = get_object_or_404(ToDoItem, pk=toDoItemId)
+    context = {
+        'toDoItem': toDoItem,
+    }
+    if request.method == "POST":
+        toDoListItemTitle = request.POST['to_do_list_item_title']
+        toDoListItemDueDate = request.POST['to_do_list_item_due_date']
+        toDoListItemDescription = request.POST['to_do_list_item_description']
+        if toDoListItemTitle == "":
+            responseList.append("please give title to the item")
+        if toDoListItemDueDate == "":
+            responseList.append("please give due date to the item")
+        else:
+            datetime_object = datetime.datetime.strptime(toDoListItemDueDate, '%Y-%m-%d').date()
+            if datetime_object < datetime.datetime.now().date():
+                responseList.append("due date must be after today")
+        if len(responseList) > 0:
+            context["responses"] = responseList
+            return render(request, 'toDoList/todolistitemupdate_form.html', context)
+        else:
+            toDoItem = get_object_or_404(ToDoItem, pk=toDoItemId)
+            toDoItem.to_do_item_title=toDoListItemTitle
+            toDoItem.to_do_due_date=toDoListItemDueDate
+            toDoItem.to_do_description=toDoListItemDescription
+            toDoItem.to_do_list_id=toDoListId
+            toDoItem.save()
+            return HttpResponseRedirect(reverse('toDoListDetail', args=[toDoListId]))
+    else:
+        return render(request, 'toDoList/todolistitemupdate_form.html', context)
+def deleteToDoListItem(request, toDoListId, toDoItemId):
+    if request.method == "POST":
+        toDoItem = get_object_or_404(ToDoItem, pk=toDoItemId)
+        toDoItem.delete()
+    return HttpResponseRedirect(reverse('toDoListDetail', args=[toDoListId]))
